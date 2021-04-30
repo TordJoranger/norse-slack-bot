44ac6a251f707617178768076232e9eca11f07f2
Tord Joranger <tord@norsefeedback.no>
2021-04-30 14:40:05 +0200

    new route

---
+++ src/main/kotlin/com/example/MessageRoute.kt
@@ -0,0 +1,37 @@
+package com.example
+
+import com.slack.api.Slack
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import jdk.nashorn.internal.objects.NativeFunction.function
+
+fun Routing.messageRoute() {
+    route("/message"){
+        post {
+            val payload = call.request
+
+            call.respond(200)
+            val token = System.getenv("SLACK_TOKEN")
+            val slack = Slack.getInstance()
+            val response = slack.methods(token).chatPostMessage {
+                it.channel("#bot")
+                    .text("God morgen til deg og!:")
+            }
+        }
+
+
+    }
+//    post("/message") {
+////        val payload:
+////        res.sendStatus(200)
+//
+//        val token = System.getenv("SLACK_TOKEN")
+//        val slack = Slack.getInstance()
+//        val response = slack.methods(token).chatPostMessage {
+//        it.channel("#bot")
+//            .text("God morgen til deg og!:")
+//    }
+//        call.respondText("Response is: $response")
+//    }
+}
